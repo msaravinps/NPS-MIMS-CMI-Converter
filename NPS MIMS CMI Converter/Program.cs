b720f8c1f3c877388a731402946a072a26fa2f07
Mark Saravi <MSaravi@nps.org.au>
2017-02-28 15:33:02 +1100

    using config file

--- NPS MIMS CMI Converter/Program.cs
+++ NPS MIMS CMI Converter/Program.cs
@@ -8,17 +8,19 @@ namespace NPS_MIMS_CMI_Converter
 {
     class Program
     {
+        public static object ConfigurationManager { get; private set; }
+
         static void Main(string[] args)
         {
-            var cmiXmlPath = @"Z:\MSaravi\MIMS-Sample\CMI\CMIxml20170100\CMIxml";
-            var xmiXmlExtension = "*.xml";
-            var schema = new Dictionary<string, string> {
-                {"CMIcode","int" },
-                {"CMIxml","string" },
-                {"CMIxmlescaped","string" }
-            };
-            var _CsvGenerator = new CsvGenerator(@"Z:\MSaravi\MIMS-Sample\CMI\output.csv", schema, "\t", "~");
-            var fileNames = Directory.EnumerateFiles(cmiXmlPath, xmiXmlExtension).ToList();
+            var cmiXmlPath = args[0];// @"Z:\MSaravi\MIMS-Sample\CMI\CMIxml20170100\CMIxml";
+            var outputPath = args[1];//@"Z:\MSaravi\MIMS-Sample\CMI\output.csv"
+            var xmiXmlExtension = @System.Configuration.ConfigurationManager.AppSettings["fileType"];// "*.xml";
+            var separator = @System.Configuration.ConfigurationManager.AppSettings["separator"];// "*.xml";
+            var enclose = @System.Configuration.ConfigurationManager.AppSettings["enclose"];// "*.xml";
+            var newLine = @System.Configuration.ConfigurationManager.AppSettings["newLine"];// "*.xml";
+            var schema = GetSchema();
+            var _CsvGenerator = new CsvGenerator(outputPath, schema, separator, enclose, newLine);
+            var fileNames = System.IO.Directory.EnumerateFiles(cmiXmlPath, xmiXmlExtension).ToList();
             var progress = 0.0;
             var total = (double)fileNames.Count;
             _CsvGenerator.AddHeader();
@@ -30,6 +32,8 @@ namespace NPS_MIMS_CMI_Converter
                 _CsvGenerator.AddRecord(new string[] { cmiCode, xmlFileContents, escapedXmlFileContents });
                 Console.WriteLine(cmiCode + " finished, %" + Math.Round(progress * 100 / total, 1).ToString());
                 progress++;
+                if (progress > 3)
+                    break;        
             }
             _CsvGenerator.End();
         }
@@ -41,5 +45,16 @@ namespace NPS_MIMS_CMI_Converter
             return Convert.ToInt64(code).ToString();
         }
 
+        static Dictionary<string, string> GetSchema()
+        {
+            var keys = @System.Configuration.ConfigurationManager.AppSettings["schema:keys"].Split(new char[] { ',' }).Select(s => s.Trim()).ToArray();
+            var types = @System.Configuration.ConfigurationManager.AppSettings["schema:types"].Split(new char[] { ',' }).Select(s => s.Trim()).ToArray();
+            var schema = new Dictionary<string, string>();
+            for (var i = 0; i < keys.Length; i++)
+            {
+                schema.Add(keys[i], types[i]);
+            }
+            return schema;
+        }
     }
 }
